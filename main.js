cdeb6a52eac37e7bf9775d867032a04b11081880
KuchkoraliyevNodirbek <nodirbekqochqarlaiyev@gmail.com>
2024-08-23 17:53:43 +0500

    finsh

---
+++ main.js
@@ -0,0 +1,21 @@
+"use strict";
+const obj = {
+    id: 1,
+    name: "Leanne Graham",
+    username: "Bret",
+    email: "Sincere@april.biz",
+    address: {
+        street: "Kulas Light",
+        suite: "Apt. 556",
+        city: "Gwenborough",
+        zipcode: 929983874,
+        geo: {
+            lat: true,
+            lng: "81.1496",
+        },
+    },
+};
+const x = (obj) => {
+    console.log(obj);
+};
+x(obj);
